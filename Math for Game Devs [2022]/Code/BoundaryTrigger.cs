16a59bad52939db9a214764421d76a3e9b8a50a8
ab5ent <inf.ng.nam@proton.me>
2024-06-18 17:07:46 +0700

    MATH FOR GAME DEVS

---
+++ Math for Game Devs [2022]/Code/BoundaryTrigger.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class BoundaryTrigger : MonoBehaviour
+{
+    private float nextCheckTime = 0;
+
+    [SerializeField]
+    private float checkInterval = 0.5f;
+
+    [SerializeField]
+    private float boundaryRadius = 1;
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private float distanceFromTarget;
+
+    private bool isInsideBoundary = false;
+
+    private void Update()
+    {
+        if (Time.time > nextCheckTime)
+        {
+            nextCheckTime = Time.time + checkInterval;
+            CheckByDistance(target.position);
+        }
+    }
+
+    private void CheckByDistance(Vector3 targetPosition)
+    {
+        distanceFromTarget = Vector3.Distance(target.position, transform.position);
+        isInsideBoundary = distanceFromTarget <= boundaryRadius;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, 1);
+
+        if (target)
+        {
+            if (nextCheckTime == 0)
+            {
+                CheckByDistance(target.position);
+            }
+            
+            Gizmos.color = isInsideBoundary ? Color.red : Color.green;
+            Gizmos.DrawLine(transform.position, target.position);
+        }
+    }
+}
---
+++ Math for Game Devs [2022]/Math for Game Devs [2022].md
@@ -0,0 +1,48 @@
+## Part 1
+
+### Number line, Vector, fundamental
+
+	Magnitude(a) = Abs(a) = |a| >= 0
+	
+	Distance(a, b) = Abs(b - a) = |b - a| >= 0
+	
+	Sign(a) = a / |a| = 1 (a > 0) or -1 (a < 0) or 0 (a = 0)
+	
+	Signed(a, b) = +1 (b > a) or -1 (b < a) or 0 (a = b)
+
+### 2D Vectors
+
+- x-axis y-axis
+- A(x, y) 
+- Represent: Position, Direction, Velocity, Offset
+- vector a (3, 2) + vector b (-4, 1) = vector c (-1, 3)
+- a - b = d; b - a = -d (all vector)
+- Vector don't have origin, just has the end point
+
+### Vector length (magnitude)
+
+
+$$
+Length (a, b)  = Magnitude(a, b)= \sqrt{(a^2 + b^2)}
+$$
+
+$$
+Distance(a, b) = ||a, b|| = \sqrt{(a^2 + y^2)}
+$$
+
+### Vector Normalization
+
+- Circle Ox (-1, 1), Oy (-1, 1)
+- Magnitude = 1
+- Normalize Vector = Unit Vector
+- Represent direction
+
+$$
+
+Direction(\overrightarrow{a}) = Normalize(\overrightarrow{a}) = \overrightarrow{a}  / ||a||
+
+$$
+
+## Part 2
+
+### 
